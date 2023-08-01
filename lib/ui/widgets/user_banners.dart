deec253e7d54e8c78cc84504e630c198544c744a
Mostafejur Rahman <mostafejur.smuct.cse@gmail.com>
2023-08-02 00:08:19 +0600

    the problem was in the app bar, sizedBox er parent hisabe Expandend use koray error asche

--- lib/ui/widgets/user_banners.dart
+++ lib/ui/widgets/user_banners.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:task_manager_flutter/data/models/auth_utility.dart';
 import 'package:task_manager_flutter/ui/screens/auth_screens/login_screen.dart';
+
 AppBar userBanner(context, {VoidCallback? onTapped}) {
   return AppBar(
     // centerTitle: true,
@@ -37,44 +38,41 @@ AppBar userBanner(context, {VoidCallback? onTapped}) {
       ),
     ],
     title: Center(
-      child: Expanded(
-        child: SizedBox(
-          height: 40,
-          width: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GestureDetector(
-                onTap: onTapped,
-                child: CircleAvatar(
-                  radius: 25,
-                  backgroundImage: const NetworkImage(
-                      "https://lh3.googleusercontent.com/a/AAcHTtcDcIjcAxYbE61EV4D7MPqUxP8TAu7elGBuYFxdmRYAa9M=s288-c-no"),
-                  onBackgroundImageError: (_, __) {
-                    const Icon(FontAwesomeIcons.solidCircleUser);
-                  },
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: onTapped,
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: const NetworkImage(
+                    "https://lh3.googleusercontent.com/a/AAcHTtcDcIjcAxYbE61EV4D7MPqUxP8TAu7elGBuYFxdmRYAa9M=s288-c-no"),
+                onBackgroundImageError: (_, __) {
+                  const Icon(FontAwesomeIcons.solidCircleUser);
+                },
+              ),
+            ),
+            const SizedBox(width: 15),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "${AuthUtility.userInfo.data?.firstName ?? " "} ${AuthUtility.userInfo.data?.lastName}",
+                  style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
                 ),
-              ),
-              const SizedBox(width: 15),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "${AuthUtility.userInfo.data?.firstName ?? " "} ${AuthUtility.userInfo.data?.lastName}",
-                    style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                  const SizedBox(height: 2),
-                  Text(AuthUtility.userInfo.data?.email ?? "",
-                      style:
-                          const TextStyle(fontSize: 14, color: Colors.white)),
-                ],
-              ),
-            ],
-          ),
+                const SizedBox(height: 2),
+                Text(AuthUtility.userInfo.data?.email ?? "",
+                    style: const TextStyle(fontSize: 14, color: Colors.white)),
+              ],
+            ),
+          ],
         ),
       ),
     ),
