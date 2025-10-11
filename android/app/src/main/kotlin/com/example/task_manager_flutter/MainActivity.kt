d17379c2aa42a5622a86fbec9b403a22f3bcf4d6
Washington climaco <wlclimaco@hotmail.com>
2025-10-11 11:55:57 -0300

    ghfghfghfg fghfghfghfgh

--- android/app/src/main/kotlin/com/example/task_manager_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/task_manager_flutter/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.task_manager_flutter
+package com.washingtonclimaco.task_manager_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
